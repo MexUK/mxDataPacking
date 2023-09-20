e2686a8e77651d0c4f2c96538b305da111a51e79
Mex <mexuk@hotmail.co.uk>
2023-09-20 02:56:19 +0100

    generic

--- src/EEndian.h
+++ src/EEndian.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstdint>
+
 #include "mx.h"
 
 enum mx::EEndian : uint8_t
--- src/EStorage.h
+++ src/EStorage.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstdint>
+
 #include "mx.h"
 
 enum mx::EStorage : uint8_t
